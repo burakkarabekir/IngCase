f3f5d8a27ae7a156061ac61e9fa780d8864d56f2
Burak Karabekir <burak.karabekir@ceng.deu.edu.tr>
2021-08-18 13:36:31 +0300

    implemented AppModule.kt

---
+++ app/src/main/java/com/burakks/ingcase/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.burakks.ingcase.di
+
+import android.content.Context
+import com.burakks.ingcase.TheApp
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context):
+            TheApp = app as TheApp
+}
