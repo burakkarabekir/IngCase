8ad5fbcbbd9be8a430d8e617872a1ee657e83b30
Burak Karabekir <burak.karabekir@ceng.deu.edu.tr>
2021-08-18 11:41:35 +0300

    init project

---
+++ app/src/main/java/com/burakks/ingcase/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.burakks.ingcase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.burakks.ingcase.ui.theme.IngCaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IngCaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    IngCaseTheme {
+        Greeting("Android")
+    }
+}
